3bcb1e3b0db0670b873cc32d4908058df67eb82f
Anastaseh <anastase4300@gmail.com>
2023-07-15 03:51:49 -0700

    changes added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
-#!/bin/bash
-int main() {
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void) {
     printf("Size of char: %zu byte(s)\n", sizeof(char));
     printf("Size of int: %zu byte(s)\n", sizeof(int));
     printf("Size of long: %zu byte(s)\n", sizeof(long int));
