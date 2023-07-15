86b70403c27858efe14cbb6aa5e70d3de38db732
Anastaseh <anastase4300@gmail.com>
2023-07-15 06:42:49 -0700

    readynow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,4 @@
 #include<stdio.h>
-/**
-*main - Entry point
-*Return: return zero indicate that the program executed without any errors 
-*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
