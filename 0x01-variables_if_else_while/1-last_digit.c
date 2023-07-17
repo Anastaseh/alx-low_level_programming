3791e798ce8531611730895d505b8356422b94de
Anastaseh <anastase4300@gmail.com>
2023-07-17 07:59:24 -0700

    corr4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - entry point 
-*
-* retun: always retun 0 
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
